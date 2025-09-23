2ed65ee7c65406a4628f56b528bf6ddf6db161d2
mattew8 <mattew4483@likelion.org>
2025-09-23 21:20:53 +0900

    refactor: switch lamp motion exports to modular entry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export { LampMotion, useLampMotionContext } from "./LampMotion";
-export type { LampMotionContextValue } from "./LampMotion";
-export type { LampMotionOpenTarget, LampMotionOptions, LampMotionOrigin } from "./types";
+export { LampMotion } from "./components/lamp-motion/LampMotion";
+export { useLampMotionContext } from "./components/lamp-motion/context";
+export type { LampMotionContextValue } from "./components/lamp-motion/context";
+export type {
+  LampMotionTriggerProps,
+  LampMotionPortalProps,
+  LampMotionContentProps,
+} from "./components/lamp-motion/LampMotion";
